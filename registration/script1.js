1ef9fbe7110b85ac9547919d66a2c9b06e48a471
Rohith <rrohith958@gmail.com>
2023-05-09 15:11:11 +0530

    final

---
+++ registration/script1.js
@@ -0,0 +1,25 @@
+document.getElementById("submit-btn").style.display = 'none';
+const btn =document.querySelector('#red-btn');
+btn.addEventListener('click',function(event){
+document.getElementById('submit-btn').style.display = 'block';
+
+});
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
